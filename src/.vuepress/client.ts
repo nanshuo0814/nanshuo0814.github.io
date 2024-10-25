226a8d6676ba3c5806339fa92ee12b8a2871d62c
南烁 <nanshuo.icu@qq.com>
2024-10-26 00:46:36 +0800

    update

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,18 +1,18 @@
 import {defineClientConfig} from "vuepress/client";
 import {setupTransparentNavbar} from "vuepress-theme-hope/presets/transparentNavbar.js";
 import {setupRunningTimeFooter} from "vuepress-theme-hope/presets/footerRunningTime.js";
-import { setupSnowFall } from "vuepress-theme-hope/presets/SnowFall.js";
+// import { setupSnowFall } from "vuepress-theme-hope/presets/SnowFall.js";
 export default defineClientConfig({
     setup: () => {
         // 下雪
-        setupSnowFall(
-            {
-                count: 15,
-                minSize: 10,
-                maxSize: 20,
-                speed: 1,
-                // image: "/ns.png"
-            });
+        // setupSnowFall(
+        //     {
+        //         count: 15,
+        //         minSize: 10,
+        //         maxSize: 20,
+        //         speed: 1,
+        //         // image: "/ns.png"
+        //     });
         // 透明导航栏
         setupTransparentNavbar({type: "all"});
         // 运行时间
