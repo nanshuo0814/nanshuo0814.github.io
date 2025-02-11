506731b4839c8fc86f989efa57da536d642f10e0
南烁 <nanshuo.icu@qq.com>
2025-02-11 12:10:11 +0800

    build: 添加 vuepress 插件以提升文章页阅读体验，添加了引流功能，关注公众号获取验证码解锁所有博客文章
    - 新增 vuepress-plugin-readmore-popular-next 插件
    - 该插件通常用于文章页面，提供相关文章推荐和继续阅读功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,6 +2,7 @@ import {defineUserConfig} from "vuepress";
 import {getDirname, path} from "vuepress/utils";
 
 import theme from "./theme.js";
+import { readmorePlugin } from 'vuepress-plugin-readmore-popular-next';
 
 const __dirname = getDirname(import.meta.url);
 
@@ -14,7 +15,42 @@ export default defineUserConfig({
     description: "记录生活、学习和工作的点点滴滴，与君共勉！",
 
     theme,
-
+    
+    plugins: [
+        // 引流插件
+        readmorePlugin({
+            // 已申请的博客 ID
+            blogId: '28519-4839232997594-788',
+            // 已申请的微信公众号名称
+            name: '烁烁南光',
+            // 已申请的微信公众号回复关键词
+            keyword: '验证码',
+            // 已申请的微信公众号二维码图片
+            qrcode: 'https://www.nanshuo.icu/wxgzh.jpg',
+            // 文章内容的 JS 选择器，若使用的不是官方默认主题，则需要根据第三方的主题来设置
+            selector: 'div.theme-hope-content',
+            // 自定义的 JS 资源链接，可用于 CDN 加速
+            libUrl: 'https://qiniu.techgrow.cn/readmore/dist/readmore.js',
+            // 自定义的 CSS 资源链接，可用于适配不同风格的博客
+            cssUrl: 'https://qiniu.techgrow.cn/readmore/dist/vuepress2.css',
+            // 文章排除添加引流工具的 URL 规则，支持使用路径、通配符、正则表达式的匹配规则
+            excludes: { strExp: [], regExp: [] },
+            // 是否反转 URL 排除规则的配置，即只有符合排除规则的文章才会添加引流工具
+            reverse: false,
+            // 文章内容的预览高度(例如 300)，设置值为 auto 则表示预览高度自适应
+            height: 'auto',
+            // 是否添加微信公众号引流工具到移动端页面
+            allowMobile: true,
+            // 文章解锁后凭证的有效天数
+            expires: 3,
+            // 定时校验凭证有效性的时间间隔（秒）
+            interval: 60,
+            // 等待 DOM 节点加载完成的时间（毫秒），如果部分页面的引流功能无法生效，可适当增大此参数的值
+            waitDomMills: 1000,
+            // 每篇文章随机添加引流工具的概率，范围在 0.1 ~ 1.0 之间，代表 10% ~ 100%，其中 1.0 表示所有文章默认都添加引流工具
+            random: 1.0
+        })
+    ],
     markdown: {
         headers: {
             // 用到哪一级就提取哪一级
